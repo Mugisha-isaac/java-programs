37a9c84a37c14ed1ef1e18a281b7f26722ac1b61
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-02-26 08:04:10 +0200

    oop beginning

--- Arrays/Three.java
+++ Arrays/Three.java
@@ -13,10 +13,20 @@ public class Three {
 
         // printing text
         String text = "He likes to play football";
-        String[] splitData = text.split(" ");
+        // String[] splitData = text.split(" ");
 
-        for (int i = 0; i < splitData.length; i++) {
-            System.out.println(splitData[i]);
+        // for (int i = 0; i < splitData.length; i++) {
+        // System.out.println(splitData[i]);
+        // }
+
+        String storage = "";
+        for (int i = 0; i < text.length() - 1; i++) {
+            if (text.charAt(i + 1 == '\0')) {
+                storage += text.charAt(i);
+                System.out.println("splitted words are: " + storage);
+            }
+
         }
+
     }
 }
